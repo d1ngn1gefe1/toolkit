0f183a5c913c11f4977e15f42aaf718da4045f96
Alan Luo <luozl1993@gmail.com>
2017-08-02 21:46:11 +0000

    make opencv (uncomment)

--- opencv.sh
+++ opencv.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-#apt-get -y install build-essential cmake git
-#apt-get -y install pkg-config unzip ffmpeg qtbase5-dev python-dev python3-dev python-numpy python3-numpy
-#apt-get -y install libopencv-dev libgtk-3-dev libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff5-dev libjasper-dev
-#apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
-#apt-get -y install libv4l-dev libtbb-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev
-#apt-get -y install libvorbis-dev libxvidcore-dev v4l-utils python-vtk
-#apt-get -y install liblapacke-dev libopenblas-dev checkinstall
-#apt-get -y install libgdal-dev
+apt-get -y install build-essential cmake git
+apt-get -y install pkg-config unzip ffmpeg qtbase5-dev python-dev python3-dev python-numpy python3-numpy
+apt-get -y install libopencv-dev libgtk-3-dev libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff5-dev libjasper-dev
+apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+apt-get -y install libv4l-dev libtbb-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev
+apt-get -y install libvorbis-dev libxvidcore-dev v4l-utils python-vtk
+apt-get -y install liblapacke-dev libopenblas-dev checkinstall
+apt-get -y install libgdal-dev
 
-#cd ~/downloads
-#wget -O opencv.zip https://github.com/opencv/opencv/archive/3.2.0.zip
-#wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.2.0.zip
-#unzip opencv.zip -d ~/downloads/opencv
-#unzip opencv_contrib.zip -d ~/downloads/opencv_contrib
+cd ~/downloads
+wget -O opencv.zip https://github.com/opencv/opencv/archive/3.2.0.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.2.0.zip
+unzip opencv.zip -d ~/downloads/opencv
+unzip opencv_contrib.zip -d ~/downloads/opencv_contrib
 
 cd ~/downloads/opencv
 mkdir build
