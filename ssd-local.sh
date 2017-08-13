67f585dcc05e48271aa01626b0bfee5f00bfc44b
Alan Luo <luozl1993@gmail.com>
2017-08-13 15:54:19 -0700

    fix bug in ssd-local

--- ssd-local.sh
+++ ssd-local.sh
@@ -6,6 +6,6 @@ chmod 777 /mnt/ssd-local
 
 apt-get update && apt-get -y install mdadm --no-install-recommend
 mdadm --create /dev/md0 --level=0 --raid-devices=8 /dev/disk/by-id/nvme-nvme.1ae0-6e766d655f6361726400-6e766d655f6361726400-00000001 /dev/disk/by-id/nvme-nvme.1ae0-6e766d655f6361726400-6e766d655f6361726400-00000002 /dev/disk/by-id/nvme-nvme.1ae0-6e766d655f6361726400-6e766d655f6361726400-00000003 /dev/disk/by-id/nvme-nvme.1ae0-6e766d655f6361726400-6e766d655f6361726400-00000004 /dev/disk/by-id/nvme-nvme.1ae0-6e766d655f6361726400-6e766d655f6361726400-00000005 /dev/disk/by-id/nvme-nvme.1ae0-6e766d655f6361726400-6e766d655f6361726400-00000006 /dev/disk/by-id/nvme-nvme.1ae0-6e766d655f6361726400-6e766d655f6361726400-00000007 /dev/disk/by-id/nvme-nvme.1ae0-6e766d655f6361726400-6e766d655f6361726400-00000008
-mkfs.ext4 -F /dev/md0s
+mkfs.ext4 -F /dev/md0
 
 mount -a
